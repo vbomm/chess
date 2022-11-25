24096dbaf35e72a0ec075aa65c7647a7877bd934
vbomm <vitus.bomm@gmail.com>
2022-11-25 12:55:14 +0100

    added comment, removed NONE

--- src/chess/controller/PieceType.java
+++ src/chess/controller/PieceType.java
@@ -1,5 +1,8 @@
 package chess.controller;
 
+/**
+ * Possible chess piece types.
+ */
 public enum PieceType {
-    PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, NONE
+    PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
 }
