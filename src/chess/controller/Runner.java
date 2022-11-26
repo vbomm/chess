a3948b8e22f43cb7195ff08d928196b791c5531f
vbomm <vitus.bomm@gmail.com>
2022-11-26 16:56:08 +0100

    added comment

--- src/chess/controller/Runner.java
+++ src/chess/controller/Runner.java
@@ -1,7 +1,15 @@
 package chess.controller;
 
+/**
+ * Main class.
+ */
 public class Runner {
 
+    /**
+     * Entry point.
+     *
+     * @param args an array of command-line arguments for the application, not used
+     */
     public static void main(String... args) {
         new Controller();
     }
